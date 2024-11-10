cfeae866ad8119323f5e7472db9eaf33f9d61db4
Cedric Ko <yacmov@gmail.com>
2024-11-10 23:32:15 +0800

    chore: header and footer moved to layout.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,3 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
-import '../../public/assets/css/bootstrap.min.css';
-import '../../public/assets/css/style.css';
-let logo = '/assets/images/logo.png';
-
 export default function Home() {
-  return (
-    <>
-      <Header logo={logo} />
-      <Footer logo={logo} />
-    </>
-  );
+  return <></>;
 }
