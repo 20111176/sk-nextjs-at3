4f35e9c72fa51a020776b8c0fdbc627104755867
Cedric Ko <yacmov@gmail.com>
2024-11-10 23:23:56 +0800

    fix(style): removed missing files from css for compile code error

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import '../../public/assets/css/bootstrap.min.css';
+import '../../public/assets/css/style.css';
 let logo = '/assets/images/logo.png';
 
 export default function Home() {
